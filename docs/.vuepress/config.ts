bbdb336dedd45c1040449a4a64da221007542ae1
acongm <acongm@126.com>
2022-01-08 00:30:59 +0800

    ci: 调整项目文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,7 @@ const { path } = require('@vuepress/utils')
 const isProduction = process.env.NODE_ENV === 'production'
 
 // 兼容 GitHub Pages 和 vercel 部署
-const base = process.env.BUILD_ENV ? process.env.BUILD_ENV : '/vuepress/'
+const base = '/vuepress/'
 
 export default defineUserConfig<DefaultThemeOptions>({
   base,
@@ -15,16 +15,18 @@ export default defineUserConfig<DefaultThemeOptions>({
   title: 'Acongm',
   description: '前端常用知识、踩坑记录、软件推荐等',
   head: [
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
+    ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
+    ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
+
+    ['link', { rel: 'manifest', href: `/manifest.webmanifest` }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     [
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
     ],
-    ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
-    ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
     [
       'script',
