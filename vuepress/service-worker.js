eb34513fe4a51c2c58d162e70f7afc3bfa9ebd03
acongm <acongm@126.com>
2025-04-26 00:19:54 +0800

    feta: build

--- vuepress/service-worker.js
+++ vuepress/service-worker.js
@@ -50,9 +50,9 @@ define('./service-worker.js', ['./workbox-f7715658'], function (s) {
   }),
     s.precacheAndRoute(
       [
-        { url: '404.html', revision: '27fbd0e0239d2f8f68ec1a998d6a4e35' },
+        { url: '404.html', revision: '4861c6bef8acab2b7bf209a4a479db18' },
         { url: 'assets/css/7339.styles.efb6fd39.css', revision: null },
-        { url: 'assets/css/styles.86e107fd.css', revision: null },
+        { url: 'assets/css/styles.de7fafc6.css', revision: null },
         { url: 'assets/img/back-to-top.8b37f773.svg', revision: null },
         { url: 'assets/img/instasll.8c70bf30.jpg', revision: null },
         { url: 'assets/img/search.b017a09f.svg', revision: null },
@@ -61,12 +61,16 @@ define('./service-worker.js', ['./workbox-f7715658'], function (s) {
         { url: 'assets/js/3616.923c7839.js', revision: null },
         { url: 'assets/js/5205.d8806728.js', revision: null },
         { url: 'assets/js/9363.1015719a.js', revision: null },
-        { url: 'assets/js/app.9267af72.js', revision: null },
-        { url: 'assets/js/runtime~app.c4bd9219.js', revision: null },
+        { url: 'assets/js/app.b694bd09.js', revision: null },
+        { url: 'assets/js/runtime~app.60a34f69.js', revision: null },
         { url: 'assets/js/v-02b49bb5.9a8f508b.js', revision: null },
+        { url: 'assets/js/v-0b126c08.11109a0e.js', revision: null },
         { url: 'assets/js/v-0d6991cf.e629ccb0.js', revision: null },
+        { url: 'assets/js/v-0e7c1d46.a552ce76.js', revision: null },
         { url: 'assets/js/v-1128ff8f.c720f63a.js', revision: null },
         { url: 'assets/js/v-134daec4.26694552.js', revision: null },
+        { url: 'assets/js/v-171543d1.44d0fbb4.js', revision: null },
+        { url: 'assets/js/v-184df6f4.6fdaccf4.js', revision: null },
         { url: 'assets/js/v-1993c2d1.b6557e38.js', revision: null },
         { url: 'assets/js/v-1df9b70c.d78c2c80.js', revision: null },
         { url: 'assets/js/v-29e2a472.79abd11d.js', revision: null },
@@ -75,7 +79,7 @@ define('./service-worker.js', ['./workbox-f7715658'], function (s) {
         { url: 'assets/js/v-2d633f67.cdd21da6.js', revision: null },
         { url: 'assets/js/v-314212cf.2c674199.js', revision: null },
         { url: 'assets/js/v-3448eb71.79074e3f.js', revision: null },
-        { url: 'assets/js/v-349853b9.393e9c63.js', revision: null },
+        { url: 'assets/js/v-349853b9.c5151a29.js', revision: null },
         { url: 'assets/js/v-3706649a.b5e9cbac.js', revision: null },
         { url: 'assets/js/v-3a1bce8b.3a1a4d8b.js', revision: null },
         { url: 'assets/js/v-3b303508.e9a0ebfb.js', revision: null },
@@ -87,6 +91,7 @@ define('./service-worker.js', ['./workbox-f7715658'], function (s) {
         { url: 'assets/js/v-4bbf47d6.6d2e5e90.js', revision: null },
         { url: 'assets/js/v-4c78588a.807f79d1.js', revision: null },
         { url: 'assets/js/v-568d3b89.25e01e5b.js', revision: null },
+        { url: 'assets/js/v-5b914f22.83369b05.js', revision: null },
         { url: 'assets/js/v-5f5e8084.27e8a010.js', revision: null },
         { url: 'assets/js/v-6657f713.baefd661.js', revision: null },
         { url: 'assets/js/v-6908e54b.b80d8794.js', revision: null },
@@ -95,13 +100,14 @@ define('./service-worker.js', ['./workbox-f7715658'], function (s) {
         { url: 'assets/js/v-6dcf6fe2.dc9d40b7.js', revision: null },
         { url: 'assets/js/v-73bc6b79.3a7d0bff.js', revision: null },
         { url: 'assets/js/v-74458d05.5999c6d2.js', revision: null },
-        { url: 'assets/js/v-74473916.382828bf.js', revision: null },
+        { url: 'assets/js/v-74473916.3268801b.js', revision: null },
         { url: 'assets/js/v-744e35e2.ae61c708.js', revision: null },
         { url: 'assets/js/v-759eeb6d.696cc5eb.js', revision: null },
         { url: 'assets/js/v-77d59f2c.a9698ac2.js', revision: null },
         { url: 'assets/js/v-7a5ee91d.98ad2949.js', revision: null },
         { url: 'assets/js/v-7bd3de6f.87df0615.js', revision: null },
-        { url: 'assets/js/v-8daa1a0e.979f74c1.js', revision: null },
+        { url: 'assets/js/v-7c3320bc.90d05923.js', revision: null },
+        { url: 'assets/js/v-8daa1a0e.c7517eb1.js', revision: null },
         { url: 'assets/js/v-8e6560ec.ff969146.js', revision: null },
         { url: 'assets/js/v-9588fb76.06e6c0cd.js', revision: null },
         { url: 'assets/js/v-ac6bd2fe.c290fd62.js', revision: null },
@@ -111,161 +117,185 @@ define('./service-worker.js', ['./workbox-f7715658'], function (s) {
         { url: 'assets/js/v-e687fcb4.408a2006.js', revision: null },
         { url: 'assets/js/v-eb8ea600.ee70f399.js', revision: null },
         { url: 'assets/js/v-f20d4d92.bbbfa957.js', revision: null },
-        { url: 'css/index.html', revision: 'da9bcda2af25839a4b60cdf80af0f682' },
-        { url: 'css/scss.html', revision: '2c53f6a6b87421bf753b98ba78fd9d80' },
-        { url: 'css/skill.html', revision: '15fee979d3320464772a7f16a052cbc4' },
+        { url: 'css/index.html', revision: '5ae9a9bd23293d190bab5591225aa81a' },
+        { url: 'css/scss.html', revision: 'af32c37aa0aa459ebd93b9b37d2b5da5' },
+        { url: 'css/skill.html', revision: 'e46735861d019abb116e92787ed1c9bd' },
         {
           url: 'git/command.html',
-          revision: 'f512d3bb63c73e62499fe5e3e5cea49b'
+          revision: 'e3f424d0e8dc035d1ee45aa18bb52454'
         },
         {
           url: 'git/commit.html',
-          revision: 'd84990924a6a2e31f02f28d2cb101425'
+          revision: '0e70c8dbfb2130c99951c3d7b626eff4'
         },
-        { url: 'git/index.html', revision: 'ff66ad34fe1fac09990e67b2f36a8bfa' },
-        { url: 'index.html', revision: '0dc048ec6df7bf93bd174f646534605f' },
-        { url: 'issue/h5.html', revision: '2981dc70295e5a5b1e2c72bc12833078' },
-        { url: 'issue/pc.html', revision: 'b3902f2b3aa6bb3195a23a9075328349' },
+        { url: 'git/index.html', revision: '0609d2605960b54944f658fc6d85617c' },
+        { url: 'index.html', revision: '43cee6a81dd7fad6893a04ec9d9eafc0' },
+        { url: 'issue/h5.html', revision: '58ca8f70dc19aa3ee60174d936d74c11' },
+        { url: 'issue/pc.html', revision: '783d9c1a5a63c445e2f5f5efef6ae9d6' },
         {
           url: 'JavaScript/call、apply与bind.html',
-          revision: 'ebf221079b625c81e75983cf6165bda0'
+          revision: 'c9ce9690192c77aaaaa8fae483aa32fe'
         },
         {
           url: 'JavaScript/index.html',
-          revision: '24afa26e2551f5ba1d9929422cde04f2'
+          revision: '6dad7411b608762e8862f32640dafb6e'
         },
         {
           url: 'JavaScript/js模拟bind方法.html',
-          revision: 'ca6e1c341ca613c6a763e844534df476'
+          revision: '8ae670b1b2acd0a98d37fd6d529664f8'
         },
         {
           url: 'JavaScript/js模拟new操作.html',
-          revision: 'd050b7091ad313b35d43090761bc86f7'
+          revision: 'c1429c7942e8205de1ae4db95b31e512'
         },
         {
           url: 'JavaScript/执行上下文-作用域链-闭包.html',
-          revision: 'b3d96e6b3034be8b838d88e9d0a6db43'
+          revision: '88138f58ce8a9ab07b304aade553df90'
         },
         {
           url: 'JavaScript/经典闭包处理.html',
-          revision: 'bb1e014fab3242a06201f48c4e8627fe'
+          revision: '0df8ee9076ed422a3c04d752d9d6113e'
+        },
+        {
+          url: 'job-description/index.html',
+          revision: '3776fe33fd2d5819f4af8c686c986275'
+        },
+        {
+          url: 'job-description/web前端开发工程师-彭聪-软通.html',
+          revision: 'ab2b900b564c458b93be52361b943e8c'
+        },
+        {
+          url: 'job-description/web前端开发工程师-彭聪.html',
+          revision: '6423d6d700b1806454a36e15c21ed529'
+        },
+        {
+          url: 'job-description/简历.html',
+          revision: '27a12828ea08c6cf245ed0f13614f0de'
         },
         { url: 'logo.jpg', revision: 'bc708b3d27886ad0589bf5ea7ea96a2f' },
-        { url: 'node/npm.html', revision: '0dedc790249b6d58e10039a8a467792e' },
+        { url: 'node/npm.html', revision: 'fc375f4160206bd1db53ab1581a837f9' },
         {
           url: 'node/toolkit.html',
-          revision: '0a442429ebbf5b4abf40400f873ddf8e'
+          revision: '1d03fc142c7782dd4b26fb4b591cf439'
         },
         {
           url: 'online-tools/bookmark-scripts.html',
-          revision: '4624ec99359b563f393f9eed24654430'
+          revision: 'f327c9ba132dd78106c84c9cfbd1aca0'
         },
         {
           url: 'online-tools/index.html',
-          revision: '2c3980c9c8e8f8567c99e11adf5aa84b'
+          revision: 'dc1540f996619528dce7c5b1cbd7a7d5'
         },
         {
           url: 'react/class-hooks.html',
-          revision: 'bf9aa23eead82de5f669a3d7bd7e8047'
+          revision: '9fb90486abc6144c773f6f53376abf02'
+        },
+        {
+          url: 'react/react17.html',
+          revision: 'cc1d9ed991735894545ff07a8cf567d7'
+        },
+        {
+          url: 'react/react18.html',
+          revision: '4e48a580bd9617633ab20d6e9e07f979'
         },
         {
           url: 'software/browser.html',
-          revision: 'c3a4d9c5283981430923efab607f365c'
+          revision: '96c18fb0fb3e5d7080c3c52cb902ad8e'
         },
         {
           url: 'software/cross-platform.html',
-          revision: 'f275d7458be10953bcd2fca1116d5d75'
+          revision: '3ce7f6b7ed96b9c083692ddfc93eb683'
         },
         {
           url: 'software/mac.html',
-          revision: 'c4349b2cdad8aadf5a1170c8b9a2c90b'
+          revision: 'aa596f86dc9eaab0c4f02ded4ea1a731'
         },
         {
           url: 'software/vscode.html',
-          revision: '4f4d8fc6bf7d2dcd33e3be4e8ee7aac6'
+          revision: '615d7b46dd7943e3db7aab3613d6f893'
         },
         {
           url: 'software/webstorm.html',
-          revision: '98ddac6bb017257ed6d11da96394ddb1'
+          revision: 'd44e1da8f8abf488c68777c85dd94062'
         },
         {
           url: 'software/windows.html',
-          revision: '229000d29d917427fe6b0eeb622aa7f6'
+          revision: '59b7901b0decd966ee549582160bcb8a'
         },
         {
           url: 'software/zsh.html',
-          revision: '99fef2266955d1f5fcc74557291bd2e6'
+          revision: '3bf5760706d995670e23a9c3f410a600'
         },
         {
           url: 'theory/index.html',
-          revision: '95b9ae2c5c7cfcd920a362feccaabe2e'
+          revision: '60f078cfe54d6a2e360f28397947ec62'
         },
         {
           url: 'TypeScript/index.html',
-          revision: 'c95b77d29b497df832d552956c6159c4'
+          revision: '350fee8e9d423ff2c74cb69a605a3941'
         },
         {
           url: 'utils/function.html',
-          revision: '8282ff89454259803a5870173b3472c6'
+          revision: '2306bae3cd216c0d44c2f95f7aebf382'
         },
         {
           url: 'utils/library.html',
-          revision: 'ba2efc8ee081fef79a7832db683f1ce5'
+          revision: 'aef1f3227699d9975e9fb2924fd02e4e'
         },
         {
           url: 'utils/regexp.html',
-          revision: 'debf3bbef687dfc351cd1ea3eaa63a71'
+          revision: 'b79cdfad1c6323f07861e8eb21967a87'
         },
         {
           url: 'vue/code/Object.defineproperty.html',
-          revision: '5a4a923b1a991c3038806aec8aa34e74'
+          revision: '745650960853072928cbeb4d8059273e'
         },
         {
           url: 'vue/code/proxy-observe.html',
-          revision: '97a81117b5e47cb01f49cf68408ec42b'
+          revision: '39a2f047fbcf596ae76e499e182996b7'
         },
         {
           url: 'vue/code/Proxy.html',
-          revision: '5891bf8d86a5a6897baa4289d4034575'
+          revision: '1c197bc066139e801fd1ac4fc6e64492'
         },
-        { url: 'vue/index.html', revision: 'b9d561c8757f1257fb5d0587a6f2c28e' },
+        { url: 'vue/index.html', revision: '8fcbca599bd732aa60aa239dc789e2d0' },
         {
           url: 'vue/vue_interview.html',
-          revision: 'd778b2ae13452f4f85b2738b12350374'
+          revision: '182628625cb61d44b7037029052d4f98'
         },
         {
           url: 'vue/vue_theory.html',
-          revision: '0d33a391f793a0a9dbaea5bc3f32c177'
+          revision: 'f106f7b660b73d6e4082f2ba112c6101'
         },
-        { url: 'vue/vue2.html', revision: '4ab24462641144a5f794ed6291ba7da6' },
-        { url: 'vue/vue3.html', revision: 'b21687866010f7464a943f2d70d8ce84' },
+        { url: 'vue/vue2.html', revision: '2ed41c557ba08f78172486d035410a03' },
+        { url: 'vue/vue3.html', revision: '642e69991816eb24bcd6b102fc2e7030' },
         {
           url: 'webpack/install/index.html',
-          revision: '245434761b48534403d559e16099516b'
+          revision: '7d8b993dd6478d7b55457b7489c6cf49'
         },
         {
           url: 'webpack/install/ni.html',
-          revision: '4facbe972fbf0674c46fd79b7ef25f20'
+          revision: '8e2c8e96f07c952fbad7752e41370b84'
         },
         {
           url: 'webpack/install/npm.html',
-          revision: '0225e9b2cf794bf266a31fee35176be3'
+          revision: 'f92ac8c68847a5bde582ed473ec1ced3'
         },
         {
           url: 'webpack/install/pnpm.html',
-          revision: 'e43812c2e700ddfe9be0b21ddcdd8c52'
+          revision: 'da59ec4d2364241bd4bd79eb97015046'
         },
         {
           url: 'webpack/install/yarn.html',
-          revision: '4facbe972fbf0674c46fd79b7ef25f20'
+          revision: '8e2c8e96f07c952fbad7752e41370b84'
         },
         {
           url: 'webpack/打包工具.html',
-          revision: '4facbe972fbf0674c46fd79b7ef25f20'
+          revision: '8e2c8e96f07c952fbad7752e41370b84'
         },
         {
           url: 'webpack/知识梳理.html',
-          revision: '852a419bb2d557d7f5ea1d235b154a64'
+          revision: 'faecf314dfef3875b7c3b7ed8f307f85'
         }
       ],
       {}
