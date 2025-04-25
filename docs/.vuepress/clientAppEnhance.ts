608cc69c9a14716c07003d6dad82864839b50e4b
acongm <acongm@126.com>
2025-04-26 03:35:48 +0800

    feta: 调整md 样式

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -1,6 +1,9 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 import ScriptX from 'vue-scriptx'
 
+import './styles/typora-theme-lapis-v1.2.1/lapis.css'
+import './styles/lapis-style.css'
+
 export default defineClientAppEnhance(({ app }) => {
   app.use(ScriptX)
 })
