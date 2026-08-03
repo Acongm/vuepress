f70cb76ad6e4da62eb43b57d07d3b80a60eaefcd
acongm <acongm@126.com>
2026-08-03 09:43:37 +0800

    docs: add daily tech news for 2026-08-03

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -243,6 +243,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           {
             text: '每日资讯',
             children: [
+              '/daily-news/2026-08-03.md',
               '/daily-news/2026-07-31.md',
               '/daily-news/2026-07-30.md',
               '/daily-news/2026-07-29.md',
@@ -569,6 +570,7 @@ export default defineUserConfig<DefaultThemeOptions>({
         {
           text: '每日资讯',
           children: [
+            '/daily-news/2026-08-03.md',
             '/daily-news/2026-07-31.md',
             '/daily-news/2026-07-30.md',
             '/daily-news/2026-07-29.md',
