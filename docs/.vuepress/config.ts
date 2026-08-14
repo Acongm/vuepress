ec2d22141cb70dcb28c919cf3f5016edd8c378d2
acongm <acongm@126.com>
2026-08-14 09:42:09 +0800

    docs: add daily news for 2026-08-14

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -243,6 +243,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           {
             text: '每日资讯',
             children: [
+              '/daily-news/2026-08-14.md',
               '/daily-news/2026-08-13.md',
               '/daily-news/2026-08-12.md',
               '/daily-news/2026-08-11.md',
@@ -578,6 +579,7 @@ export default defineUserConfig<DefaultThemeOptions>({
         {
           text: '每日资讯',
           children: [
+            '/daily-news/2026-08-14.md',
             '/daily-news/2026-08-13.md',
             '/daily-news/2026-08-12.md',
             '/daily-news/2026-08-11.md',
